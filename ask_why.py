a9785147d3ffb552b54830ad83c621ee7dacb347
engineer <engineer@fed1764-csauto.jaspersoft.com>
2013-09-20 04:15:11 -0700

    added 2 simple Py files

---
+++ ask_why.py
@@ -0,0 +1,8 @@
+#simply check if number entered is more than 10 or not
+
+x = raw_input('enter integer number:\n')
+
+if int(x) < 10:
+	print 'less than 10'
+elif int(x) >= 10:
+	print 'more than 10 or 10'
